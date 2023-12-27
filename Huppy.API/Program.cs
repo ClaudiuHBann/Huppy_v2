4ed0d159f9dc2e8a662cdc985e4db361e489791c
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-12-27 05:55:50 +0200

    fixed cors for the huppy browser

--- Huppy.API/Program.cs
+++ Huppy.API/Program.cs
@@ -3,6 +3,7 @@ using Huppy.API.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<HuppyContext>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +15,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// TODO: fix CORS
+app.UseCors(config => config.AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .SetIsOriginAllowed(origin => true)
+#if DEBUG
+                          .WithOrigins("https://localhost:7194/")
+#else
+                          .WithOrigins("https://162.55.32.18:80/")
+#endif
+                          .AllowCredentials());
 
 app.UseHttpsRedirection();
 app.UseAuthorization();

94e9408d353adf7314efc307f59c0bcf231bc213
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-12-27 03:29:52 +0200

    renamed utilities to services and removed the static shit

---
+++ Huppy/Services/DI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Huppy.Utilities
+{
+public static class DI
+{
+    private static readonly ServiceProvider _services = new ServiceCollection()
+                                                            .AddSingleton<DatabaseService>()
+                                                            .AddSingleton<NotificationService>()
+                                                            .BuildServiceProvider();
+
+    public static Type Create<Type>(params object[] parameters)
+    {
+        return ActivatorUtilities.CreateInstance<Type>(_services, parameters);
+    }
+}
+}
---
+++ Huppy/Services/NotificationService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Avalonia.Controls.Notifications;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace Huppy.Utilities
+{
+public class NotificationService
+{
+    private readonly WindowNotificationManager? _manager = null;
+
+    private ObservableCollection<INotification>? _cache = [];
+
+    public NotificationService()
+    {
+        if (Avalonia.Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            _manager = new(desktop.MainWindow);
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+
+        _manager.Position = NotificationPosition.BottomRight;
+        _manager.MaxItems = 2;
+
+        _manager.TemplateApplied += (sender, args) =>
+        {
+            if (_cache == null)
+            {
+                return;
+            }
+
+            foreach (var notification in _cache)
+            {
+                _manager?.Show(notification);
+            }
+
+            _cache.Clear();
+            _cache = null;
+        };
+    }
+
+    private void Notify(Notification notification)
+    {
+        if (_cache != null)
+        {
+            _cache.Add(notification);
+        }
+        else
+        {
+            _manager?.Show(notification);
+        }
+    }
+
+    public void NotifyI(string message) => Notify(new("Huppy", message, NotificationType.Information));
+    public void NotifyE(string message) => Notify(new("Huppy", message, NotificationType.Error));
+}
+}
